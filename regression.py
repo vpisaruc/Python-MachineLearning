3079f8128b0cfc29b9a1afacd56f4934f42b5b53
vpisaruc <vpisaruc@gmail.com>
2019-07-03 19:23:46 +0300

    Regression forecasting and predicting

--- regression.py
+++ regression.py
@@ -1,10 +1,14 @@
 import sklearn as sk
-import quandl, math
+import quandl, math, datetime
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 """Linear regression"""
 df = quandl.get('WIKI/GOOGL')
@@ -28,16 +32,17 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 
 # shift - смещение значений колонки
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 # features или значения по которым мы пытаемся предугадать значения
 X = np.array(df.drop(['label'], 1))
-# our labels или значения которыу мы пытаемся предугадать
-y = np.array(df['label'])
-
 # маштабируем наши данные
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
 df.dropna(inplace=True)
+# our labels или значения которыу мы пытаемся предугадать
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 # функция train_test_split разбивает наш масивв данных на 4 подмассива,
@@ -51,10 +56,25 @@ clf = LinearRegression()
 clf.fit(X_train, y_train)
 # считаем насколько наша система хорошо предсказываает значения
 accuracy = clf.score(X_test, y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
 
-print(accuracy)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
 
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
 
-
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 
 

f2318ef09c4e89c1439a57113daa9f4ca3d22b84
vpisaruc <vpisaruc@gmail.com>
2019-07-03 17:56:13 +0300

    Regression Features and Labels

--- regression.py
+++ regression.py
@@ -1,6 +1,7 @@
 import sklearn as sk
 import quandl
 import pandas as pd
+import math
 
 """Linear regression"""
 df = quandl.get('WIKI/GOOGL')
@@ -9,10 +10,22 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
+# значения по которым мы пытаемся предугадать значения
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
+# столбец который мы пытаемся предугадать
+forecast_col = 'Adj. Close'
 
+# Машинное обучение может работать с NaN значениями, если им присвоенно какоето значение
+# в нашем случаем - 99999 или нужно избавиться от NaN(не советую, т.к можете потярть много данных)
+df.fillna(-99999, inplace=True)
 
+# ceil - округление в большую сторону
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+# shift - смещение значений колонки
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 
 
 

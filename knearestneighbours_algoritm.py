1fc5c3762eb18b7774571f063e3396a590fd0c22
vpisaruc <vpisaruc@gmail.com>
2019-07-11 15:26:26 +0300

    Final thoughts on K Nearest Neighbors

--- knearestneighbours_algoritm.py
+++ knearestneighbours_algoritm.py
@@ -30,39 +30,46 @@ def k_nearest_neighbours(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:3]]
     # смотрим к какой группе принадлежит большинство, к этой же группе и будет принадлежать наша точка
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return vote_result
+    return vote_result, confidence
 
-df = pd.read_csv(r'Datasets\breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-full_data = df.astype(float).values.tolist()
-# смешаем данные
-random.shuffle(full_data)
+accurasies = []
 
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
+for i in range(5):
+    df = pd.read_csv(r'Datasets\breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+    # смешаем данные
+    random.shuffle(full_data)
 
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
+    test_size = 0.4
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
 
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
 
-correct = 0
-total = 0
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbours(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        total += 1
+    correct = 0
+    total = 0
 
-print('Accuracy: ', correct/total)
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbours(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+
+            total += 1
+
+    accurasies.append(correct/total)
+
+print(sum(accurasies)/len(accurasies))
 
 # result = k_nearest_neighbours(dataset, new_features, k=3)
 

f0357056bb6219c40be5fa733ada2d09ce1452f5
vpisaruc <vpisaruc@gmail.com>
2019-07-20 15:30:43 +0300

    SVM optimization

--- supportvectormachine_algoritm.py
+++ supportvectormachine_algoritm.py
@@ -44,7 +44,8 @@ class Support_Vector_Machine:
 
         # extremely expensive
         b_range_multiple = 5
-
+        # we don't need to take as small of steps
+        # with b as we do w
         b_multiple = 5
 
         latest_optimum = self.max_feature_value * 10
@@ -55,8 +56,40 @@ class Support_Vector_Machine:
             # we can do this because convex
             optimized = False
             while not optimized:
-                pass
 
+                for b in np.arange(-1 * (self.max_feature_value * b_range_multiple),
+                                   self.max_feature_value * b_range_multiple,
+                                   step * b_multiple):
+                    for transformation in transforms:
+                        w_t = w * transformation
+                        found_option = True
+                        # weakest link in the SVM fundamentally
+                        # SMO attempts to fix this a bit
+                        # yi(xi*w+b) >=1
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi * (np.dot(w_t, xi) + b) >=1:
+                                    found_option = False
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step. ')
+                else:
+                    # w = [5, 5]
+                    # step = 1
+                    # w - step = [4, 4]
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])
+            # ||w|| : [w, b]
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step * 2
 
 
 

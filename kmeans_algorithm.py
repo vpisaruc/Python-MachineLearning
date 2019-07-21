644ee793afe7758a15b5baaf7dccd330ddcdf4a4
vpisaruc <vpisaruc@gmail.com>
2019-07-21 16:34:57 +0300

    Corrected some errors

--- kmeans_algorithm.py
+++ kmeans_algorithm.py
@@ -3,18 +3,6 @@ from matplotlib import style
 import numpy as np
 style.use('ggplot')
 
-X = np.array([[1, 2],
-              [1.5, 1.8],
-              [5, 8],
-              [8, 8],
-              [1, 0.6],
-              [9, 11]])
-
-# plt.scatter(X[:, 0], X[:, 1], s=150)
-# plt.show()
-
-colors = ['g', 'r', 'c', 'b', 'k']
-
 
 class K_Means:
     # k - колличество групп на которые будут разбиваться данные
@@ -26,46 +14,68 @@ class K_Means:
         self.max_iter = max_iter
 
     def fit(self, data):
-
+        # словарь центров наших двух групп, т.к k=2
         self.centroids = {}
 
+        # предположим, что центры это первые две точки нашего массива точек
         for i in range(self.k):
             self.centroids[i] = data[i]
 
+        # выполняем цикл, пока не найдены подходящие центры или не достигнуто max_iter
         for i in range(self.max_iter):
+            # словарь классификация наших групп
             self.classifications = {}
 
+            # заполняем наш словарь пустыми массивами
             for i in range(self.k):
                 self.classifications[i] = []
 
+            # вычисляем дистанцию между центроидами и точкой и смотрим к какому центру ближе точка
+            # относим точку к той или инной группе в зависимости от того к какому центру она ближе
             for featureset in data:
                 distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
                 classification = distances.index(min(distances))
                 self.classifications[classification].append(featureset)
 
+            # значение предыдущих центров
             prev_centroids = dict(self.centroids)
 
+            # перевычисляем центры групп
             for classification in self.classifications:
                 self.centroids[classification] = np.average(self.classifications[classification], axis=0)
             optimized = True
 
+
             for c in self.centroids:
                 original_centroids = prev_centroids[c]
                 current_centroid = self.centroids[c]
+                # если наш центр превышает допустимое смещение tol, циклимся дальше
                 if np.sum((current_centroid - original_centroids)/original_centroids * 100) > self.tol:
                     optimized = False
 
+            # если значение tol не превышенное, выходим из цикла
             if optimized:
                 break
 
-
-
-
     def predict(self, data):
+        # вычисляем к какому центру ближе точка и присваиваем этой точке группу
         distances = [np.linalg.norm(data - self.centroids[centroid]) for centroid in self.centroids]
         classification = distances.index(min(distances))
         return classification
 
+
+
+X = np.array([[1, 2],
+              [1.5, 1.8],
+              [5, 8],
+              [8, 8],
+              [1, 0.6],
+              [9, 11]])
+
+# plt.scatter(X[:, 0], X[:, 1], s=150)
+# plt.show()
+
+colors = ['g', 'r', 'c', 'b', 'k']
 
 clf = K_Means()
 clf.fit(X)
